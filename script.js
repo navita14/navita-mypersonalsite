6a58de466d59bc50dbf0d3e140df1412c3fdb7bf
Navita Ramasar <navitaramasar14@gmail.com>
2023-08-14 20:36:50 -0400

    New Edits

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// h2.textContent = "Thank you for visiting my page!";
 document.querySelector("body").appendChild(h2);
+
+
+function myContent(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
